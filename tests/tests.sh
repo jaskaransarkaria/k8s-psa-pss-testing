f2ebf1b7711e8dfacae1e51dc03856d557282387
jaskaransarkaria <jaskaran.sarkaria@digital.justice.gov.uk>
2023-09-20 14:28:36 +0100

    refactor: 💡 and add valid pod

--- tests/tests.sh
+++ tests/tests.sh
@@ -12,40 +12,51 @@ SERVICEACCOUNT_TOKEN=$1
 
 echo "${NEWLINE}"
 
+
+if [[ -z "$SERVICEACCOUNT_TOKEN" ]]; then
+    echo "Pass you service account token in ./tests.sh <service_account_token>"
+    exit 1
+fi
+
 echo ">>> 1. Good config..."
 "${CMD_KUBECTL}" apply -f "${PREFIX}"1-ok.yaml --token="${SERVICEACCOUNT_TOKEN}"
 sleep 2
-"${CMD_KUBECTL}" delete -f "${PREFIX}"1-ok.yaml --token="${SERVICEACCOUNT_TOKEN}"
+"${CMD_KUBECTL}" delete -f "${PREFIX}"1-ok.yaml
 sleep 2
 
 echo "${NEWLINE}"
 
 echo ">>> 2. Deployment - Missing container security context element..."
 "${CMD_KUBECTL}" apply -f "${PREFIX}"2-dep-sec-cont.yaml --token="${SERVICEACCOUNT_TOKEN}"
+"${CMD_KUBECTL}" delete -f "${PREFIX}"2-dep-sec-cont.yaml
 sleep 2
 
 echo "${NEWLINE}"
 
 echo ">>> 3. Pod - Missing container security context element..."
 "${CMD_KUBECTL}" apply -f "${PREFIX}"3-pod.yaml --token="${SERVICEACCOUNT_TOKEN}"
+"${CMD_KUBECTL}" delete -f "${PREFIX}"3-pod.yaml
 sleep 2
 
 echo "${NEWLINE}"
 
 echo ">>> 4. Pod - Pod security context, but Missing container security context element..."
 "${CMD_KUBECTL}" apply -f "${PREFIX}"4-pod.yaml --token="${SERVICEACCOUNT_TOKEN}"
+"${CMD_KUBECTL}" delete -f "${PREFIX}"4-pod.yaml
 sleep 2
 
 echo "${NEWLINE}"
 
 echo ">>> 5. Pod - Container security context element present, with incorrect settings..."
 "${CMD_KUBECTL}" apply -f "${PREFIX}"5-pod.yaml --token="${SERVICEACCOUNT_TOKEN}"
+"${CMD_KUBECTL}" delete -f "${PREFIX}"5-pod.yaml
 sleep 2
 
 echo "${NEWLINE}"
 
 echo ">>> 6. Pod - Container security context element present, with incorrect spec.hostNetwork, spec.hostPID, spec.hostIPC settings..."
 "${CMD_KUBECTL}" apply -f "${PREFIX}"6-pod.yaml --token="${SERVICEACCOUNT_TOKEN}"
+"${CMD_KUBECTL}" delete -f "${PREFIX}"6-pod.yaml
 sleep 2
 
 echo "${NEWLINE}"
